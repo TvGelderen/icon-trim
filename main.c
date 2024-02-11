cbc344360d149915d4f0bbf776137a4830ce932a
TvGelderen <tvgelderen@outlook.com>
2024-02-11 11:20:07 +0100

    added jq functionality

--- main.c
+++ main.c
@@ -1,36 +1,9 @@
-/* We want POSIX.1-2008 + XSI, i.e. SuSv4, features */
-#define _XOPEN_SOURCE 700
-
-/* Added on 2017-06-25:
-   If the C library can support 64-bit file sizes
-   and offsets, using the standard names,
-   these defines tell the C library to do so. */
-#define _LARGEFILE64_SOURCE
-#define _FILE_OFFSET_BITS 64
-
-#include <errno.h>
-#include <ftw.h>
+#include <ctype.h>
+#include <dirent.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <time.h>
+#include <sys/types.h>
 #include <unistd.h>
-
-/* POSIX.1 says each process has at least 20 file descriptors.
- * Three of those belong to the standard streams.
- * Here, we use a conservative estimate of 15 available;
- * assuming we use at most two for other uses in this program,
- * we should never run into any problems.
- * Most trees are shallower than that, so it is efficient.
- * Deeper trees are traversed fine, just a bit slower.
- * (Linux allows typically hundreds to thousands of open files,
- *  so you'll probably never see any issues even if you used
- *  a much higher value, say a couple of hundred, but
- *  15 is a safe, reasonable value.)
- */
-#ifndef USE_FDS
-#define USE_FDS 15
-#endif
 
 int fileContainsString(const char *filepath, const char *icon) {
   char command[256];
@@ -45,7 +18,7 @@ int fileContainsString(const char *filepath, const char *icon) {
 
   FILE *result = popen(command, "r");
   if (result == NULL) {
-    return errno;
+    return 0;
   }
 
   char output[256];
@@ -56,53 +29,82 @@ int fileContainsString(const char *filepath, const char *icon) {
   return 1;
 }
 
-int handleFile(const char *filepath, const struct stat *info,
-               const int typeflag, struct FTW *pathinfo) {
-  switch (typeflag) {
-  case FTW_F:
-    printf(" %s\n", filepath);
-    // TODO: Get icon names from input file
-    if (fileContainsString(filepath, "icon-name")) {
-      printf("file contains string\n\n");
-    }
-    break;
-  case FTW_D:
-  case FTW_DP:
-    printf(" %s/\n", filepath);
-    break;
-  default:
-    printf(" %s (unknown)\n", filepath);
-    break;
-  }
+void listdir(const char *name) {
+  DIR *dir;
+  struct dirent *entry;
 
-  return 0;
+  if (!(dir = opendir(name)))
+    return;
+
+  while ((entry = readdir(dir)) != NULL) {
+    if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
+      continue;
+    }
+
+    char path[1024];
+    snprintf(path, sizeof(path), "%s/%s", name, entry->d_name);
+    printf("%s\n", path);
+
+    if (entry->d_type == DT_DIR) {
+      listdir(path);
+    } else {
+      if (fileContainsString(path, "icon-name")) {
+        printf("file contains string\n\n");
+      } else {
+        printf("file does not contain string\n\n");
+      }
+    }
+  }
+  closedir(dir);
 }
 
-int traverseFiles(const char *const dirpath) {
-  int result;
+void getIconNames(const char *filePath) {
+  char command[256];
+  memset(command, 0, 256);
+  strcpy(command, "jq '.icons | keys' ");
+  strncat(command, filePath, strlen(filePath));
 
-  if (dirpath == NULL || *dirpath == '\0') {
-    return errno = EINVAL;
+  printf("jq command: %s\n\n", command);
+
+  FILE *result = popen(command, "r");
+
+  char line[256];
+  while (fgets(line, 256, result)) {
+    if (line[0] != '[' && line[0] != ']') {
+      char icon[256];
+      memset(icon, 0, 256);
+      int idx = 0;
+      int iconLenth = 0;
+
+      while (!isalpha(line[idx])) {
+        idx++;
+      }
+
+      for (; idx < strlen(line); idx++) {
+        if (line[idx] == '\"') {
+          break;
+        }
+        icon[iconLenth++] = line[idx];
+      }
+
+      printf("%s\n", icon);
+    }
   }
-
-  result = nftw(dirpath, handleFile, USE_FDS, FTW_PHYS);
-  if (result >= 0) {
-    errno = result;
-  }
-
-  return errno;
 }
 
 int main(int argc, char *argv[]) {
-  char baseDir[256];
+  char iconFilePath[256];
+  char startDir[256];
 
-  printf("Please enter the directory containing the code to scan: ");
-  scanf("%s", baseDir);
+  printf("Please enter the path to the icon file: ");
+  scanf("%s", iconFilePath);
 
-  if (traverseFiles(baseDir)) {
-    fprintf(stderr, "%s.\n", strerror(errno));
-    return EXIT_FAILURE;
-  }
+  getIconNames(iconFilePath);
 
-  return EXIT_SUCCESS;
+  printf("Please enter the directory to scan: ");
+  scanf("%s", startDir);
+
+  listdir(startDir);
+
+  return 0;
 }
